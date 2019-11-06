3d90cf8d1f6d52d4fd2d3bb2b72f74b0159f3d27
Neil Campbell <neildcampbell@hotmail.com>
2019-11-06 05:44:00 +0000

    project setup

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import SlotMachine from './SlotMachine.js'
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <SlotMachine />
+            </div>
+        )
+    }
+}
